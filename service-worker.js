e27009dc5f3da2428832bc21a068dd3ffe3f0e61
AinhoaxVi <cespedesainhoa1@gmail.com>
2026-06-27 17:46:09 +0200

    v9.6.2

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,6 @@
-const CACHE_NAME = "ventas-sph-v10-pendientes";
+const CACHE_NAME = "ventas-sph-v10-2";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null)))); self.clients.claim(); });
 self.addEventListener("fetch", event => { event.respondWith(caches.match(event.request).then(response => response || fetch(event.request))); });
+self.addEventListener("notificationclick", event => { event.notification.close(); event.waitUntil(clients.matchAll({type:"window", includeUncontrolled:true}).then(list => { for(const client of list){ if("focus" in client) return client.focus(); } if(clients.openWindow) return clients.openWindow("./"); })); });
