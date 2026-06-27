4cfa83ce2b619b009c9feba924ba0af0ff92cd3a
AinhoaxVi <cespedesainhoa1@gmail.com>
2026-06-27 16:25:08 +0200

    v9.4.3

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ventas-sph-v9-7-1-rescate";
+const CACHE_NAME = "ventas-sph-v9-6-bv-final";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null)))); self.clients.claim(); });
