44a78e0ac9a27a156b6671501243074a00a93f03
AinhoaxVi <cespedesainhoa1@gmail.com>
2026-09-23 13:37:41 +0200

    v16: incentivos fiables, widgets alineados y calendario completo
    Corrige el cálculo y añade comprobación visible de incentivos. Reorganiza todas las pantallas sin huecos, alinea los widgets, incorpora frase diaria y vistas de calendario mensual, semanal y diaria.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ventas-pro-v15-executive-workspace";
+const CACHE_NAME = "ventas-pro-v16-calendar-incentives";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./favicon.svg", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
